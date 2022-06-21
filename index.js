323f707182e4c221c496dac4c1a5c01461f202af
Grttechie <105379551+Grttechie@users.noreply.github.com>
2022-06-21 13:34:23 -0700

    Update index.js
    JS Calculator

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-var x = window.prompt("Enter your first value:")//first input variable
-var y = window.prompt("Enter your second value")//second input variable
-var operation = window.prompt("Input '+' or '-' or '*' or '/', to perform an operation on the operands")
-var z; //result variable
-
-//using the unary plus(+) to convert the input strings to numbers
-var int_x = (+x);
-var int_y = (+y);
+let condition = true;
 
 //addition
-if (operation == "+"){
-    z = int_x + int_y;
-    window.alert("result=" + z);
+while (condition){
+    let x = window.prompt("Enter your first value:")//first input variable
+    let y = window.prompt("Enter your second value")//second input variable
+    let operation = window.prompt("Input '+' or '-' or '*' or '/', to perform an operation on the operands")
+    let z; //result variable
+
+    //using the unary plus(+) to convert the input strings to numbers
+    let int_x = (+x);
+    let int_y = (+y);
+    if (operation === "+"){
+        z = int_x + int_y;
+        window.alert("result=" + z);
+        condition = false;
+    }
+    
+    //subtraction
+    else if(operation === "-"){
+        z = int_x - int_y;
+        window.alert("result=" + z);
+        condition = false;
+    }
+    
+    //multiplication
+    else if(operation === "*"){
+        z = int_x * int_y;
+        window.alert("result=" + z);
+        condition = false;
+    }
+    
+    //division
+    else if(operation === "/"){
+        z = int_x / int_y;
+        window.alert("result=" + z);
+        condition = false;
+
+    }
+
+    else{
+        window.alert("Wrong input")
+        condition = false;
+    }
+
+    let option = prompt("Type 'c' to continue, 'q' to quit");
+    if (option == "c"){
+        condition = true;
+    }
+
+    else if (option == "q"){
+        condition = false;
+    }
+
 }
 
-//subtraction
-else if(operation == "-"){
-    z = int_x - int_y;
-    window.alert("result=" + z);
-}
-
-//multiplication
-else if(operation == "*"){
-    z = int_x * int_y;
-    window.alert("result=" + z);
-}
-
-//division
-else{
-    z = int_x / int_y;
-    window.alert("result=" + z);
-}
